2a73ce3a119f663c3bc67aa9ebd4e651bec0c6b7
David Liberal <davidliberal0@gmail.com>
2024-01-12 20:12:22 -0500

    set hashmap for ID and passwords class

---
+++ src/main/java/org/example/IDandPasswords.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class IDandPasswords {
+
+    // create hash map
+    // Key : Value
+    // Username : Password
+    HashMap<String, String> logininfo = new HashMap<String, String>();
+
+    // constructor
+    IDandPasswords() {
+        logininfo.put("Bro", "pizza");
+    }
+}
