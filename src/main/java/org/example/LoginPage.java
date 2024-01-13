a48c90aff00571d257b565ec708c1d12b203e4c6
David Liberal <davidliberal0@gmail.com>
2024-01-13 12:08:41 -0500

    add UI login page components

--- src/main/java/org/example/LoginPage.java
+++ src/main/java/org/example/LoginPage.java
@@ -1,20 +1,30 @@
 package org.example;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 public class LoginPage implements ActionListener {
 
+    // Login Page UI
+    JFrame frame = new JFrame();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+    JTextField userIDField = new JTextField();
+    JPasswordField userPasswordField = new JPasswordField();
+    JLabel userIDLabel = new JLabel("User ID:");
+    JLabel userPasswordLabel = new JLabel("Password:");
+    JLabel messageLabel = new JLabel();
+
     HashMap<String, String> logininfo = new HashMap<String, String>();
 
-    // constructor - accepts hashmap
     LoginPage(HashMap<String, String> loginInfoOriginal) {
         this.logininfo = loginInfoOriginal;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
     }
 }
