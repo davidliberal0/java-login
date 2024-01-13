b97a2196259a6d17ba559e4e2cc958298d08aa6c
David Liberal <davidliberal0@gmail.com>
2024-01-13 00:15:10 -0500

    pass login info into LoginPage and add action listeners to LoginPage

--- src/main/java/org/example/LoginPage.java
+++ src/main/java/org/example/LoginPage.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public class LoginPage {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+public class LoginPage implements ActionListener {
+
+    HashMap<String, String> logininfo = new HashMap<String, String>();
+
+    // constructor - accepts hashmap
+    LoginPage(HashMap<String, String> loginInfoOriginal) {
+        this.logininfo = loginInfoOriginal;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
 }
