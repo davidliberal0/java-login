c8f173ba44e2c44286e2ecd4ee4258b252cb2dd6
David Liberal <davidliberal0@gmail.com>
2024-01-13 12:36:06 -0500

    create layout and add components

--- src/main/java/org/example/LoginPage.java
+++ src/main/java/org/example/LoginPage.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 public class LoginPage implements ActionListener {
 
-    // Login Page UI
+    // Login Page UI --------------------------------
+
+    // Component Instantiation
     JFrame frame = new JFrame();
     JButton loginButton = new JButton("Login");
     JButton resetButton = new JButton("Reset");
@@ -15,12 +18,43 @@ public class LoginPage implements ActionListener {
     JPasswordField userPasswordField = new JPasswordField();
     JLabel userIDLabel = new JLabel("User ID:");
     JLabel userPasswordLabel = new JLabel("Password:");
-    JLabel messageLabel = new JLabel();
+    JLabel messageLabel = new JLabel("This is a text");
 
     HashMap<String, String> logininfo = new HashMap<String, String>();
 
     LoginPage(HashMap<String, String> loginInfoOriginal) {
         this.logininfo = loginInfoOriginal;
+
+        // Components Layout
+        userIDLabel.setBounds(50, 100, 75, 25);
+        userPasswordLabel.setBounds(50, 150, 75, 25);
+        messageLabel.setBounds(125, 250, 250, 35);
+        messageLabel.setFont(new Font(null, Font.ITALIC, 25));
+        userIDField.setBounds(125, 100, 200, 25);
+        userPasswordField.setBounds(125, 150, 200, 25);
+
+        // login button layout
+        loginButton.setBounds(125, 200, 100, 25);
+        loginButton.addActionListener(this);
+
+        // reset button
+        resetButton.setBounds(225, 200, 100, 25);
+        resetButton.addActionListener(this);
+
+        // Add to frame
+        frame.add(userIDLabel);
+        frame.add(userPasswordLabel);
+        frame.add(messageLabel);
+        frame.add(userIDField);
+        frame.add(userPasswordField);
+        frame.add(loginButton);
+        frame.add(resetButton);
+
+        // frame attributes
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 
     @Override
