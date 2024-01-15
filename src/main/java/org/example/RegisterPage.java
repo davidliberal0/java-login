4d075dfa3d349bad548057f68bd541c88b5c6f91
David Liberal <davidliberal0@gmail.com>
2024-01-15 17:03:38 -0500

    setup register page for component layout

--- src/main/java/org/example/RegisterPage.java
+++ src/main/java/org/example/RegisterPage.java
@@ -1,13 +1,33 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class RegisterPage {
+public class RegisterPage implements ActionListener {
 
     JFrame frame = new JFrame();
+    JLabel userIDRegisterLabel = new JLabel();
+    JLabel passwordRegisterLabel = new JLabel();
+    JTextField usernameRegister = new JTextField();
+    JTextField passwordRegister = new JTextField();
+    JButton registerUser = new JButton();
+
 
     RegisterPage() {
 
+        // Components Layout
+
+        // Text fields
+
+        // Register button
+
+        // Add to frame
+
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
