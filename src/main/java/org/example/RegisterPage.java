b0352feabfe3aaf4eac8edf18819532165c674e2
David Liberal <davidliberal0@gmail.com>
2024-01-15 17:15:30 -0500

    add register page title, frame, and frame attributes

--- src/main/java/org/example/RegisterPage.java
+++ src/main/java/org/example/RegisterPage.java
@@ -1,28 +1,42 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class RegisterPage implements ActionListener {
 
     JFrame frame = new JFrame();
-    JLabel userIDRegisterLabel = new JLabel();
-    JLabel passwordRegisterLabel = new JLabel();
+    JLabel registerPageTitle = new JLabel("Register a New User");
+    JLabel userIDRegisterLabel = new JLabel("User ID:");
+    JLabel userPasswordRegisterLabel = new JLabel("Password:");
     JTextField usernameRegister = new JTextField();
-    JTextField passwordRegister = new JTextField();
+    JPasswordField passwordRegister = new JPasswordField();
     JButton registerUser = new JButton();
 
 
     RegisterPage() {
 
         // Components Layout
-
+        registerPageTitle.setBounds(100, 50, 250, 30);
+        registerPageTitle.setFont(new Font(null, Font.ITALIC, 25));
+        userIDRegisterLabel.setBounds(50, 100, 75, 25);
+        userPasswordRegisterLabel.setBounds(50, 150, 75, 25);
         // Text fields
 
         // Register button
 
         // Add to frame
+        frame.add(userIDRegisterLabel);
+        frame.add(userPasswordRegisterLabel);
+        frame.add(registerPageTitle);
+
+        // frame attributes
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
 
     }
 
